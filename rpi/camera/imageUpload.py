30f834201b22bfc1fa1e5a942a639d8bddc66d13
Nine <chayapatlbs@gmail.com>
2021-03-25 02:55:23 +0700

    [rpi] : add camera test with button

--- rpi/camera/imageUpload.py
+++ rpi/camera/imageUpload.py
@@ -3,9 +3,9 @@ import requests
 
 # open('/Users/chayapat/Desktop/frames/2021-2-16_2_1_R.jpg', 'rb')
 
-def imageUpload(log_id = '605747214465273bf8544720' , fileBuffer = open('mock.jpg' , 'rb') ) :
+def imageUpload(log_id , fileBuffer ) :
     # TODO : change to Edge UPLOAD ENDPOINT
-    UPLOAD_ENDPOINT = 'http://localhost:5002/api/gallery/upload' 
+    UPLOAD_ENDPOINT = 'http://161.200.199.2:5002/api/gallery/upload' 
     files = {
         'image': (
             'img.jpg', 
@@ -15,4 +15,3 @@ def imageUpload(log_id = '605747214465273bf8544720' , fileBuffer = open('mock.jp
 
     data = {'log_id' : log_id}
     r1 = requests.post(UPLOAD_ENDPOINT , files = files,data = data)
-
