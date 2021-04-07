98abdf8dcf3dc3bb9ddd1cb5a57bc2bbdaaacb36
Nine <chayapatlbs@gmail.com>
2021-04-07 16:18:38 +0700

    fix eiei bug

--- server/client-endpoints/src/topics/cloud-to-client/sendTo.ts
+++ server/client-endpoints/src/topics/cloud-to-client/sendTo.ts
@@ -1,9 +1,6 @@
 import {Socket} from 'socket.io';
 
 function sendToSocketsClient(sockets : Socket[] , topic : string , data : any) {
-    console.log(sockets);
-    console.log(topic , data);
-    sockets[0].emit('eiei' , 'eiee')
     sockets.forEach((socket) => {
         socket.emit(topic,data);
     });
